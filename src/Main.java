73f7a00b3297f5bbd5e1a1d923959b16ae4bcee4
Zobiya Jumani <zobiyajumani@gmail.com>
2022-09-23 22:34:12 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+public class Main extends JFrame  {
+    Main(){
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
